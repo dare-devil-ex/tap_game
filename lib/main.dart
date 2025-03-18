1ab681008080b3c1fd200fab6ab0037caf34e967
dare-devil-ex <kmsedition@gmail.com>
2025-03-18 19:48:51 +0530

    modified:   lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,63 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(HomePage());
+  runApp(MaterialApp(home: HomePage()));
 }
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+          children: [
+      
+            Container(
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height / 2,
+              color: Colors.blueAccent,
+              child: Center(
+                child: MaterialButton(onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => GamePage()));
+                },
+                color: const Color.fromARGB(202, 255, 255, 255),
+                height: 100,
+                minWidth: 100,
+                shape: CircleBorder(),
+                child: Text(
+                  "START"
+                ),)
+              ),
+            ),
+
+            Container(
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height / 2,
+              color: Colors.redAccent,
+              child: Center(
+                child: MaterialButton(onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => GamePage()));
+                },
+                color: const Color.fromARGB(202, 255, 255, 255),
+                height: 100,
+                minWidth: 100,
+                shape: CircleBorder(),
+                child: Text(
+                  "START"
+                ),)
+              ),
+            ),
+      
+          ],
+        ),
+      );
+  }
+}
+
+
+class GamePage extends StatelessWidget{
+  const GamePage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -15,44 +67,45 @@ class HomePage extends StatelessWidget {
           children: [
       
             Container(
-              width: double.infinity,
-              height: MediaQuery.of(context).size.height / 2,
-              color: Colors.blueAccent,
-              child: Center(
-                child: Container(
-                  height: 90,
-                  width: 90,
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    color: const Color.fromARGB(202, 255, 255, 255),
-                    borderRadius: BorderRadius.circular(100)
-                  ),
-                  child: Text(
-                    "START"
-                  ),
+                width: double.infinity,
+                height: MediaQuery.of(context).size.height / 2,
+                color: Colors.blueAccent,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("PLAYER B", style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold
+                    ),),
+                    Text("50", style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold
+                    ),),
+                  ],
                 ),
-              ),
             ),
-
+      
             Container(
-              width: double.infinity,
-              height: MediaQuery.of(context).size.height / 2,
-              color: Colors.redAccent,
-              child: Center(
-                child: Container(
-                  height: 90,
-                  width: 90,
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    color: const Color.fromARGB(202, 255, 255, 255),
-                    borderRadius: BorderRadius.circular(100)
-                  ),
-                  child: Text(
-                    "START"
-                  ),
+                width: double.infinity,
+                height: MediaQuery.of(context).size.height / 2,
+                color: Colors.redAccent,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("PLAYER A", style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold
+                    ),),
+                    Text("50", style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold
+                    ),),
+                  ],
                 ),
-              ),
             ),
+      
       
           ],
         ),
